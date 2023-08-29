c8a3992cea20437b443c155f26a073c8720afe04
Alex <apelich77@gmail.com>
2023-08-29 18:29:22 +0300

    piece drag update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css'
-import { defaultPiecesTheme as dpt } from './piecesThemes.ts'
 import Game from './components/Game'
 import Theme from './components/Theme'
 
 function App() {
-    const pieces = Object.values(dpt)
-
     return (
         <Theme>
-            {pieces.map((p) => (
-                <img key={p} src={p} />
-            ))}
             <Game />
         </Theme>
     )
--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -1,5 +1,4 @@
 import './styles.scss'
-import { piecesTheme } from '../../piecesThemes.ts'
 
 export default function Tile({
     id,
@@ -10,14 +9,47 @@ export default function Tile({
     colors: string[]
     piece?: string
 }) {
+    function dragStartHandler(e: React.DragEvent, id: string) {
+        e.dataTransfer.clearData()
+        e.dataTransfer.setData('startId', id)
+    }
+
+    function dragLeaveHandler(e: React.DragEvent) {
+        e.preventDefault()
+    }
+
+    function dragEndHandler(e: React.DragEvent) {
+        e.preventDefault()
+    }
+
+    function dragOverHandler(e: React.DragEvent) {
+        e.preventDefault()
+    }
+
+    function dropHandler(e: React.DragEvent, id: string) {
+        e.preventDefault()
+        console.log(`${e.dataTransfer.getData('startId')} > ${id}`)
+    }
+
     return (
         <div
             className={`tile`}
             style={{ backgroundColor: colors[0], color: colors[1] }}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragEnd={(e) => dragEndHandler(e)}
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropHandler(e, id)}
         >
             {id[0] === 'a' && <span className="str-symbol">{id[1]}</span>}
             {id[1] === '1' && <span className="col-symbol">{id[0]}</span>}
-            {piece && <img src={piece} className="piece" draggable={false} />}
+            {piece && (
+                <img
+                    src={piece}
+                    className="piece"
+                    draggable={true}
+                    onDragStart={(e) => dragStartHandler(e, id)}
+                />
+            )}
         </div>
     )
 }
