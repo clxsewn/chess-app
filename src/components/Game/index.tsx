edc889b9fdd484f5ae6acfb72f7637d5d54663e7
Alex <apelich77@gmail.com>
2023-09-09 22:49:20 +0300

    timer added

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,12 +1,14 @@
 import Board from '../Board'
 import Aside from '../Aside'
 import './styles.scss'
+import Timer from '../Timer'
 
 export default function Game() {
     return (
         <div className="game">
             <Board />
             <Aside />
+            <Timer />
         </div>
     )
 }
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,3 +1,23 @@
+import { useTimer } from 'react-timer-hook'
+
 export default function Timer() {
-    return <div></div>
+    const time = new Date()
+    time.setSeconds(time.getSeconds() + 60) // 10 minutes timer
+    const { seconds, minutes } = useTimer({
+        expiryTimestamp: time,
+        onExpire: () => {
+            console.log('EXPIRED')
+        },
+    })
+
+    const secondsPrefix = seconds < 10 ? '0' : ''
+    const minutePrefix = minutes < 10 ? '0' : ''
+
+    return (
+        <div>
+            {minutePrefix}
+            {minutes}:{secondsPrefix}
+            {seconds}
+        </div>
+    )
 }
